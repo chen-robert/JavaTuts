ae53d0d230a2ccfb5c0cf5b68992af4d231331c0
brendonho01 <brendon.ho01@gmail.com>
2017-06-28 21:19:40 -0400

    06.01

---
+++ Mod 6/MyContactInfo.java
@@ -0,0 +1,16 @@
+/**
+ * The purpose of this program is to print a message to the screen.
+ * 
+ * �FLVS 2008 - Lesson 06.02: Code Evaluation 1
+ * @author J. Doe
+ * @version 07/10/08
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Brendon Ho");
+        System.out.println("Why Java?");
+        
+    }//end of main method
+}//end of class
