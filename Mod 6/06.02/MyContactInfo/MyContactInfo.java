cfb6bcb092f14aa8d853b13a496b22c15f6acde8
brendonho01 <brendon.ho01@gmail.com>
2017-07-01 22:28:27 -0400

    cleaned up

---
+++ Mod 6/06.02/MyContactInfo/MyContactInfo.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class MyContactInfo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyContactInfo
+{
+    public static void main(String[] args){
+        System.out.println("Student: Brendon Ho");
+        System.out.println("Home Phone: 123-456-7890");
+        System.out.println("Cell Phone: 143-234-4643");
+        System.out.println("");
+        System.out.println("Parent/Guardian: Jane Ho");
+        System.out.println("Home Phone: 125-123-2134");
+        System.out.println("Cell Phone: 155-632-1235");
+        System.out.println("Work Phone: 126-123-1235");
+        System.out.println("");
+        System.out.println("Parent/Guardian: James Ho");
+        System.out.println("Home Phone: 125-123-2634");
+        System.out.println("Cell Phone: 155-632-7835");
+        System.out.println("Work Phone: 126-123-1284");
+    }//end of main method
+}//end of class
