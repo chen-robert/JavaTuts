372c8a6263870fd7a8648599c42d21d988b527ef
brendonho01 <brendon.ho01@gmail.com>
2017-07-10 15:00:08 -0400

    removed doc

--- Mod 7/07.04/GraphIterationsOne/SurveyTester.java
+++ Mod 7/07.04/GraphIterationsOne/SurveyTester.java
@@ -9,6 +9,13 @@ import java.awt.*;
 //tester class
 class Survey{
     public static void bar(Turtle myrtle, int x1, int x2, int y1, int y2){
+        myrtle.hide();
+        myrtle.penUp();
+        myrtle.setPenWidth(10);
+        myrtle.setPenColor(Color.RED);
+        myrtle.moveTo(x1,y1);
+        myrtle.penDown();
+        myrtle.moveTo(x2,y2);
     }
 }
 
@@ -17,6 +24,8 @@ public class SurveyTester
 {
     public static void main(String[] args){
         World worldObj = new World();
+        //Picture pix = new Picture("Graph.png");
+        //worldObj.setPicture(pix);
         Turtle b = new Turtle(0,0, worldObj);
         
         Survey surv= new Survey();
