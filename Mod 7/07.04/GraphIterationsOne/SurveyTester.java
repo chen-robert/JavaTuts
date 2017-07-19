58cede985c31ac129aefde64aa34af92efb710a1
brendonho01 <brendon.ho01@gmail.com>
2017-07-19 17:05:26 -0400

    07.04 update

--- Mod 7/07.04/GraphIterationsOne/SurveyTester.java
+++ Mod 7/07.04/GraphIterationsOne/SurveyTester.java
@@ -1,16 +1,17 @@
 
 /**
- * A bar graph of 10 people and their favorite programming language.
+ * The purpose of this program is to create a graph from a survey that I had given to people
  *
- * @author Brendon Ho
- * @version 7/10/17
+ * @author (Andrew Xia)
+ * @version (07/18/2017)
  */
 import java.awt.*;
 
-//tester class
-class Survey{
+//start of tester class
+class Survey
+{//start of class
     
-    //bar method
+    //for creation of the bars on the graph
     public static void bar(Turtle myrtle, int x1, int x2, int y1, int y2, Color color){
         //myrtle properties
         myrtle.hide();
@@ -22,54 +23,61 @@ class Survey{
         myrtle.moveTo(x2,y2);
     }//end o fmethod
     
-    //conversion method
+    //convert string into chrome data
     public int convertStringToInt(String stringOfDigits)
-    {
-        int number = 0;                                                 //declare and initialize the number variable    
-        int total = 0;                                                  //declare and initialize the total variable
-        String subData = "";                                            //declare and initialize the subData variable
-          
-        for(int n = 0; n < stringOfDigits.length() ; n+=2)              //traverse the length of the loop by increments of 2
-        {
-            subData = stringOfDigits.substring(n, n+2);                 //pick out the substrings of 2 digit numbers
-            number = Integer.parseInt(subData);                         //parse a string to an integer
-            total+=number;                                              //accumulate the total
+    {//start of user-defined method 2
+        int number = 0;                                         //declare and initialize the number variable
+        int total = 0;                                          //declare and initialize the total variable
+        String subData = "";                                    //declare and initialize the subData variable
+        
+        for(int n = 0; n < stringOfDigits.length() ; n+=2)       //traverse the length of the loop by increments of 2
+        {//start of loop
+            subData = stringOfDigits.substring(n, n+2);       //pick out the substrings of 2 digit numbers
+            number = Integer.parseInt(subData);                 //parse a string to an integer
+            total += number;                                     //accumulate the total
         }//end of loop
         
-        return total;                                                   //return the total calculated
+        return total;                                           //return the total calculated
         
-    }//end of method
+    }//end of user-defined method 2
 }//end of class
 
-//Public Survey class
+//start of public class
 public class SurveyTester
-{
-    public static void main(String[] args){
-        //get the world environment set up
+{//start of class
+    public static void main(String[] args)
+    {//start of main method
+        //set up the world and environment
         World worldObj = new World();
-        Picture pix = new Picture("Graph.jpg");
-        worldObj.setPicture(pix);
-        Turtle b = new Turtle(0,0, worldObj);
+        Picture graph = new Picture("Graph.jpg");
+        worldObj.setPicture(graph);
+        Turtle b = new Turtle(0, 0, worldObj);
         
-        //get Survey set up
-        Survey surv= new Survey();
+        Survey surv = new Survey();//object of type survey
         
-        //bars
-        surv.bar(b, 105, 105, 370, 285, Color.ORANGE);
-        surv.bar(b, 205, 205, 370, 190, Color.BLACK);
-        surv.bar(b, 305, 305, 370, 360, Color.BLUE);
+        //assign the values to a string
+        String swiftPpl = "03";
+        String javaPpl = "06";
+        String pythonPpl = "01";
         
-        //strings and numbers for the convert method
-        String swiftPpl = "111";
-        String javaPpl = "111111";
-        String pythonPpl = "1";
+        //convert strings to numbers
+        int convertSwift = surv.convertStringToInt(swiftPpl);
+        int convertJava = surv.convertStringToInt(javaPpl);
+        int convertPython = surv.convertStringToInt(pythonPpl);
         
-        System.out.println("# of people who chose Swift: " + surv.convertStringToInt(swiftPpl));
-        System.out.println("# of people who chose Java: " + surv.convertStringToInt(javaPpl));
-        System.out.println("# of people who chose Python: " + surv.convertStringToInt(pythonPpl));
         
-        //Print into system the world
+        //bars for the graph
+        surv.bar(b, 105, 105, 370, (370 - (convertSwift * 30)), Color.ORANGE);
+        surv.bar(b, 205, 205, 370, (370 - (convertJava * 30)), Color.BLACK);
+        surv.bar(b, 305, 305, 370, (370 - (convertPython * 30)), Color.BLUE);
+        
+        
+        //print information to screen
         System.out.println(worldObj);
+        System.out.println("# of people who chose Swift: " + convertSwift);
+        System.out.println("# of people who chose Java: " + convertJava);
+        System.out.println("# of people who chose Python: " + convertPython);
         
-    }//end if method
+        
+    }//end of main method
 }//end of class
---
+++ Mod 8/08.03/PanoramaTester/PanoramaTester.java
@@ -0,0 +1,16 @@
+
+/**
+ * Make a stitched image of 5 mars pictures.
+ *
+ * @author Brendon Ho
+ * @version 7/19/17
+ */
+
+import java.awt.*;     //graphics
+
+public class PanoramaTester
+{
+    public static void main(){
+        System.out.println("xibba");
+    }
+}
